8c4d045cb555b40d2d82dad2bc0c8f5eee1dc119
Daanikus <daanikus@gmail.com>
2021-03-16 10:18:26 +1300

    Edit comment

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import "fmt"
 // - Playbook code block links find the code even after it's moved!
 //   Add a few lines to this description and click the "Open in..." link in the playbook again.
 //   Note the same code block is still highlighed, even after it's moved.
-// - Change the "hello world" to something else, note the code updates in the playbook.
 // - Select some code and hit cmd+shift+p to add your own code snippet to a playbook
 // - Return to the welcome playbook to keep following the instructions there
 // Happy coding!
