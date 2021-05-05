519e98c8fb39d8680ce832eaccbaff5920609524
Jesse Meek <jesse@codelingo.io>
2021-05-05 16:06:18 +1200

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-// Congrats! You've just linked through to some code from your welcome playbook!
+// Congrats! You've just linked through to some code from your Notebook!
 // The repo was cloned to your machine, the file opened and the lines highlighted automatically, pretty cool right?
 //
 // Things to try:
-// - Playbook code block links find the code even after it's moved!
-//   Add a few lines to this description and click the "Open in..." link in the playbook again.
+// - Notebook code block links find the code even after it's moved!
+//   Add a few lines to this description and click the "Open in..." link in the Notebook again.
 //   Note the same code block is still highlighed, even after it's moved.
-// - Select some code and hit cmd+shift+p to add your own code snippet to a playbook
-// - Return to the welcome playbook to keep following the instructions there
+// - Select the `selectMe` function below and hit cmd+shift+p to add the snippet to a Notebook.
+// - Return to the welcome Notebook to keep following the instructions there.
 // Happy coding!
 func main() {
 	fmt.Println("hello world")
