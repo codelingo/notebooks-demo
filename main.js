2081be6e394836a329fef10c353d5f57a02df3e2
mrcrowl <mrcrowl@gmail.com>
2021-05-21 15:21:51 +1200

    Update and rename main.go to main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// 🎉 You've been linked to this code in a demo repo! The repo was cloned to your computer.
+//
+// Next things to try:
+//
+// 1. Select the `goodbye` function below and use the shortcut Alt+P
+//    to add the snippet to a new Notebook.
+//
+//    ... ✨ Code snippets follow your code when it moves!
+// 2. Add a few lines to this description and re-click "Open snippet in..." from the Notebook webpage.
+//    Note the same code block is still highlighed, even after it's moved.
+//
+// 🙌 Happy coding! 
+
+function hello() {
+    console.log("Hello, world!");
+}
+
+function goodbye() {
+    console.warn("This file will self-destruct in 5, 4, 3, 2, .... 1! 💥");
+}
